1a2d85ecb91ce1db13497d90b8d295236f70c43f
vidhiarbat07 <arbatva@jdcoem.ac.in>
2026-09-13 15:14:55 +0530

    Add files via upload

---
+++ practical2.py
@@ -0,0 +1,21 @@
+a = 10
+b = 5
+# Arithmetic
+print("Addition:", a + b)
+print("Division:", a / b)
+# Relational
+print("a > b:", a > b)
+# Logical
+print("a > 5 and b < 10:", a > 5 and b < 10)
+# Bitwise
+print("a & b:", a & b)
+# Assignment
+a += b
+print("Updated a:", a)
+# Membership
+list1 = [1, 2, 3, 4]
+print("2 in list1:", 2 in list1)
+# Identity
+x = 10
+y = 10
+print("x is y:", x is y)
